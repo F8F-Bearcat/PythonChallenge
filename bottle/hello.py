aa615def3a970a9697657445824e58764bb15762
F8F-Bearcat <debaets@hotmail.com>
2015-10-03 19:48:01 -0700

    Working hello.py for bottle 10/3/2015
    Serving of static files works with this version as of 10/3/2105

--- bottle/hello.py
+++ bottle/hello.py
@@ -1,11 +1,32 @@
-from bottle import route, run
+from bottle import route, run, template, static_file, url
+''' A bit of documentation
+'''
+
+#home_page = open('index.html').read()
+@route('/')
+def home():
+    ''' A bit of documentation
+    '''
+    return template('index')
+
+@route('/<filename:path>')
+def send_static(filename):
+    ''' a bit of documentation
+    '''
+    return static_file(filename, root='static/')
 
 @route('/hello')
 def hello():
+    ''' A bit of documentation
+    '''
     return '<h1>Hello World!</h1>'
 
 @route('/hello/')
 def hello():
+    ''' A bit of documentation
+    '''
     return '<h1>Hello World (two slash...) !</h1>'
+
+
 
 run(host='0.0.0.0', port=8080)
